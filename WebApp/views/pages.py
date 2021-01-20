04954f4514d40ad4d0f9921e4212a16e341f86e3
Ebba Louise Toreld Fingarsen <elfingar@stud.ntnu.no>
2021-01-20 11:49:26 +0100

    send form to database with mostly set data

--- WebApp/views/pages.py
+++ WebApp/views/pages.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, Response, jsonify, render_template, request
+from flask import Flask, g, Response, jsonify, render_template, request, redirect
 from WebApp import app, get_neo_db
 from WebApp.db.db_util import *
 from WebApp.util.serialize_search import *
@@ -13,9 +13,10 @@ def home():
 
     # connect to neo4j db
     db = get_neo_db()
-    results = db.run('CALL improvedSearch.bm25Search("'+ query[0] +'") ')
+    results = db.run('CALL improvedSearch.bm25Search("'+ query[1] +'") ')
     serialized_result = [serialize_results(record) for record in results]
     form = RadioForm()
+    method ='BM25'
 
     if form.is_submitted():
         result = request.form
@@ -33,3 +34,22 @@ def index():
 @app.route('/landing')
 def landing():
     return render_template("landing.html")
+
+
+@app.route('/handleForm', methods=['GET', 'POST'])
+def handleForm():
+    print(request.form)
+    query_id = ''
+    try:
+        query_id = request.args['queryId']
+    except KeyError:
+        return redirect('/home')
+
+    query_id = int(query_id)
+    # connect to sqlite3 db
+    conn = db_connect("./db/rdf-ir.db")
+    for key,value in request.form.items():
+        add_test_result_disease(conn, 'BM25', key, value, query_id, 7)
+    # add_test_result_disease(conn, 'BM25', result_rank, relevancy, query_id, tester_id)
+
+    return redirect('/home')

6a07eb7a940a962d8512033b28e7c9418e618d53
Ebba Louise Toreld Fingarsen <elfingar@stud.ntnu.no>
2021-01-25 14:24:46 +0100

    cleanup

--- WebApp/db/neo_util.py
+++ WebApp/db/neo_util.py
@@ -11,6 +11,7 @@ def bm25f_search(db, q):
     return [serialize_results(record) for record in results]
 
 def fulltext_search(db, q):
-    results = db.run("call db.index.fulltext.queryNodes('NameDescAlias','name:"+ q +" OR altNames:" + q + " OR description:" + q + "') YIELD node, score RETURN node, score limit 10")
-    print(results)
+    results = db.run("call db.index.fulltext.queryNodes('NameDescAlias','name:"+ q +" OR altNames:" + q + " OR description:" + q + "')"+
+    " YIELD node, score " +
+    "RETURN node, score limit 10")
     return [serialize_disease(record) for record in results]

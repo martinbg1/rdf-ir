b62116939a8164f46a4a84ad2838d1856bece5f5
martinbg1 <martin.b.gjerde@gmail.com>
2021-03-03 14:06:48 +0100

    parse server response

--- WebApp/util/serialize_search.py
+++ WebApp/util/serialize_search.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 def serialize_disease(disease):
     return {
@@ -36,7 +37,17 @@ def serialize_results(res):
     try:
         node = json.loads(res['node'])
     except:
-        return 'no data'
+        try:
+            processed = re.sub(r'([A-Za-z0-9_.\-/:\s,]+)', r'"\1"', res['node']).replace('=', ':').replace(', name', '", "name').replace(', description', '", "description').replace(', uri', '", "uri')
+
+            return {
+                'name': processed['name'],
+                'description': processed['description'],
+                'altNames': processed['altNames']
+                # 'score': res['score']
+            }
+        except:
+            return "no data"
     return {
         'name': node['name'],
         'description': node['description'],

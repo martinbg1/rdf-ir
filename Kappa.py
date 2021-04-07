f3b5b6f0243810657c53628f09c30e137230ef57
Ebba Louise Toreld Fingarsen <elfingar@stud.ntnu.no>
2021-04-07 14:12:33 +0200

    WIP: added some logic for movie

--- Kappa.py
+++ Kappa.py
@@ -25,10 +25,12 @@ def db_connect(db_file):
         print(e)
     return conn
 
-def db_get_disease_testers():
+# "SELECT tester_id FROM Tester WHERE answered_disease=1"
+# "SELECT tester_id FROM Tester WHERE answered_movie=1"
+def db_get_testers(query):
     conn = db_connect(SQLITE_PATH)
     cur = conn.cursor()
-    cur.execute("SELECT tester_id FROM Tester WHERE answered_disease=1")
+    cur.execute(query)
     tester_id = [i[0] for i in cur.fetchall()]
     conn.close()
     return tester_id
@@ -45,39 +47,88 @@ def db_get_query_tester_result_disease(query_id, tester_id, models):
         result[model] = cur.fetchall()
     return result
 
-# AB_score= cohen_kappa_score(A,B, labels=None, weights=None)
-# print(AB_score)
-result = []
+def db_get_query_tester_result_movie(query_id, tester_id, models):
+    conn = db_connect(SQLITE_PATH)
+    cur = conn.cursor()
+    result = {}
+    for model in models:
+        cur.execute("""
+            SELECT result_rank, relevancy
+            FROM DataMovie 
+            WHERE query_id=? AND tester_id=? AND method=?""", (query_id, tester_id, model))
+        result[model] = cur.fetchall()
+    return result
+
+def formatting(result):
+    formatted = []
+    for j,res in enumerate(result):
+        formatted += [[j,i,res[i]] for i in range(0,len(res))]
+        # print("i for loop: " + temp)
+    return formatted
+
+# models = ["fulltext", "BM25", "BM25F"]
 models = ["fulltext"]
-disease_testers = db_get_disease_testers()
-# print(disease_testers)
+
+# CALCULATE FOR DISEASE:
+four_level_disease = []
+disease_testers = db_get_testers("SELECT tester_id FROM Tester WHERE answered_disease=1")
 for tester in disease_testers:
-    # print(tester)
     disease_result = db_get_query_tester_result_disease(5, tester, models)
-    result.append([n[1] for n in disease_result["fulltext"]])
-    # for n in disease_result['fulltext'][1]:
-print(result)
-new = []
-for n in result:
+    four_level_disease.append([n[1] for n in disease_result["fulltext"]])
+# print(four_level_disease)
+
+two_level_disease = []
+for n in four_level_disease:
     tmp = []
     for i in n:
         if i > 1:
             tmp.append(1)
         else:
             tmp.append(0)
-    new.append(tmp)
-print(new)
+    two_level_disease.append(tmp)
+# print(two_level_disease)
 
-def formatting(result):
-    temp = []
-    for j,res in enumerate(result):
-        temp += [[j,i,res[i]] for i in range(0,len(res))]
-        # print("i for loop: " + temp)
-    return temp
-
-temp = formatting(new)
-score_fleiss = agreement.AnnotationTask(data=temp)
+disease_four = formatting(four_level_disease)
+score_fleiss = agreement.AnnotationTask(data=disease_four)
+print("Disease: four level multi kappa ~")
 print(score_fleiss.multi_kappa())
+print("\t")
+
+disease_two = formatting(two_level_disease)
+score_fleiss = agreement.AnnotationTask(data=disease_two)
+print("Disease: two level multi kappa ~")
+print(score_fleiss.multi_kappa())
+
+# CALCULATE FOR MOVIE:
+four_level_movie = []
+movie_testers = db_get_testers("SELECT tester_id FROM Tester WHERE answered_movie=1")
+for tester in movie_testers:
+    movie_result = db_get_query_tester_result_movie(6, tester, models)
+    four_level_movie.append([n[1] for n in disease_result["fulltext"]])
+# print(four_level_movie)
+
+two_level_movie = []
+for n in four_level_movie:
+    tmp = []
+    for i in n:
+        if i > 1:
+            tmp.append(1)
+        else:
+            tmp.append(0)
+    two_level_movie.append(tmp)
+# print(two_level_movie)
+
+movie_four = formatting(four_level_movie)
+score_fleiss = agreement.AnnotationTask(data=movie_four)
+print("Movie: four level multi kappa ~")
+print(score_fleiss.multi_kappa())
+print("\t")
+
+movie_two = formatting(two_level_movie)
+score_fleiss = agreement.AnnotationTask(data=movie_two)
+print("Movie: two level multi kappa ~")
+print(score_fleiss.multi_kappa())
+
 
 # [[0,i,A[i]] for i in range(0,len(A))]
 

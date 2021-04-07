c2321c03fb5fa371a76b0f26b94f71bb3d5df94d
Ebba Louise Toreld Fingarsen <elfingar@stud.ntnu.no>
2021-04-07 16:42:56 +0200

    working (?) kappa for disease og movie fulltext

--- Kappa.py
+++ Kappa.py
@@ -10,6 +10,11 @@ B = [0,2,2,2,1,1,0,0,0,0]
 C = [3,3,2,2,1,1,0,0,0,0]
 D = [3,3,2,2,1,1,0,0,0,0]
 
+models = ["fulltext", "BM25", "BM25F"]
+# models = ["fulltext"]
+query_disease = [1,2,3,4,5]
+query_movie = [6,7,8,9,10]
+
 def db_connect(db_file):
     """ 
     Start a database connection to a sqlite database
@@ -25,8 +30,7 @@ def db_connect(db_file):
         print(e)
     return conn
 
-# "SELECT tester_id FROM Tester WHERE answered_disease=1"
-# "SELECT tester_id FROM Tester WHERE answered_movie=1"
+# hente alle testere
 def db_get_testers(query):
     conn = db_connect(SQLITE_PATH)
     cur = conn.cursor()
@@ -63,77 +67,110 @@ def formatting(result):
     formatted = []
     for j,res in enumerate(result):
         formatted += [[j,i,res[i]] for i in range(0,len(res))]
-        # print("i for loop: " + temp)
     return formatted
 
-# models = ["fulltext", "BM25", "BM25F"]
-models = ["fulltext"]
-
 # CALCULATE FOR DISEASE:
-four_level_disease = []
 disease_testers = db_get_testers("SELECT tester_id FROM Tester WHERE answered_disease=1")
-for tester in disease_testers:
-    disease_result = db_get_query_tester_result_disease(5, tester, models)
-    four_level_disease.append([n[1] for n in disease_result["fulltext"]])
-# print(four_level_disease)
 
-two_level_disease = []
-for n in four_level_disease:
-    tmp = []
-    for i in n:
-        if i > 1:
-            tmp.append(1)
-        else:
-            tmp.append(0)
-    two_level_disease.append(tmp)
-# print(two_level_disease)
+fleiss_scores_four = []
+def calculate_feiss_four_disease(q_id, model):
+    temp_disease =[]
+    for tester in disease_testers:
+        disease_result = db_get_query_tester_result_disease(q_id, tester, models)
+        temp_disease.append([n[1] for n in disease_result[model]])
+    formatted_disease= formatting(temp_disease)
+    fleiss_agreement = agreement.AnnotationTask(data=formatted_disease)
+    fleiss_scores_four.append(fleiss_agreement.multi_kappa())
 
-disease_four = formatting(four_level_disease)
-score_fleiss = agreement.AnnotationTask(data=disease_four)
-print("Disease: four level multi kappa ~")
-print(score_fleiss.multi_kappa())
-print("\t")
+temp_disease_two = []
+def convert_to_two_level(input):
+    for n in input:
+            tmp = []
+            for i in n:
+                if i > 1:
+                    tmp.append(1)
+                else:
+                    tmp.append(0)
+            temp_disease_two.append(tmp)
 
-disease_two = formatting(two_level_disease)
-score_fleiss = agreement.AnnotationTask(data=disease_two)
-print("Disease: two level multi kappa ~")
-print(score_fleiss.multi_kappa())
+fleiss_scores_two = []
+def calculate_feiss_two_disease(q_id, model):
+    two_temp = []
+    for tester in disease_testers:
+        disease_result = db_get_query_tester_result_disease(q_id, tester, models)
+        two_temp.append([n[1] for n in disease_result[model]])
+    convert_to_two_level(two_temp)
+    formatted_disease= formatting(temp_disease_two)
+    fleiss_agreement = agreement.AnnotationTask(data=formatted_disease)
+    fleiss_scores_two.append(fleiss_agreement.multi_kappa())
+
+def avg_fleiss(scores):
+    total = 0
+    for score in scores:
+        print(score)
+        total +=score
+    return total/len(scores)
+
+#calculate for 4 levels (fungerer tror jeg, er kun for 1 modell nå, men er avg av alle queries)
+for q_id in query_disease:
+    calculate_feiss_four_disease(q_id,"fulltext")
+
+disease_avg_four=avg_fleiss(fleiss_scores_four)
+print("disease avg four: " + str(disease_avg_four))
+
+# caculate for 2 levels (fungerer kanksje?)
+for q_id in query_disease:
+    calculate_feiss_two_disease(q_id, "fulltext")
+
+disease_avg_two=avg_fleiss(fleiss_scores_two)
+print("disease avg two: " + str(disease_avg_two))
+
 
 # CALCULATE FOR MOVIE:
-four_level_movie = []
 movie_testers = db_get_testers("SELECT tester_id FROM Tester WHERE answered_movie=1")
-for tester in movie_testers:
-    movie_result = db_get_query_tester_result_movie(6, tester, models)
-    four_level_movie.append([n[1] for n in disease_result["fulltext"]])
-# print(four_level_movie)
 
-two_level_movie = []
-for n in four_level_movie:
-    tmp = []
-    for i in n:
-        if i > 1:
-            tmp.append(1)
-        else:
-            tmp.append(0)
-    two_level_movie.append(tmp)
-# print(two_level_movie)
+fleiss_scores_four_movie = []
+def calculate_feiss_four_movie(q_id, model):
+    temp_movie =[]
+    for tester in movie_testers:
+        movie_result = db_get_query_tester_result_movie(q_id, tester, models)
+        temp_movie.append([n[1] for n in movie_result[model]])
+    formatted_movie= formatting(temp_movie)
+    fleiss_agreement = agreement.AnnotationTask(data=formatted_movie)
+    fleiss_scores_four_movie.append(fleiss_agreement.multi_kappa())
 
-movie_four = formatting(four_level_movie)
-score_fleiss = agreement.AnnotationTask(data=movie_four)
-print("Movie: four level multi kappa ~")
-print(score_fleiss.multi_kappa())
-print("\t")
+temp_movie_two = []
+def convert_to_two_level_movie(input):
+    for n in input:
+            tmp = []
+            for i in n:
+                if i > 1:
+                    tmp.append(1)
+                else:
+                    tmp.append(0)
+            temp_movie_two.append(tmp)
 
-movie_two = formatting(two_level_movie)
-score_fleiss = agreement.AnnotationTask(data=movie_two)
-print("Movie: two level multi kappa ~")
-print(score_fleiss.multi_kappa())
+fleiss_scores_two_movie = []
+def calculate_feiss_two_movie(q_id, model):
+    two_temp = []
+    for tester in movie_testers:
+        movie_result = db_get_query_tester_result_movie(q_id, tester, models)
+        two_temp.append([n[1] for n in movie_result[model]])
+    convert_to_two_level_movie(two_temp)
+    formatted_movie= formatting(temp_movie_two)
+    fleiss_agreement = agreement.AnnotationTask(data=formatted_movie)
+    fleiss_scores_two_movie.append(fleiss_agreement.multi_kappa())
 
+#calculate for 4 levels (fungerer tror jeg, er kun for 1 modell nå, men er avg av alle queries)
+for q_id in query_movie:
+    calculate_feiss_four_movie(q_id,"fulltext")
 
-# [[0,i,A[i]] for i in range(0,len(A))]
+movie_avg_four=avg_fleiss(fleiss_scores_four_movie)
+print("movie avg four: " + str(movie_avg_four))
 
+# caculate for 2 levels (fungerer kanksje?)
+for q_id in query_movie:
+    calculate_feiss_two_movie(q_id, "fulltext")
 
-# formatted_codes = [[0,i,A[i]] for i in range(0,len(A))] + [[1,i,B[i]] for i in range(0,len(B))] + [[2,i,C[i]] for i in range(0,len(C))]+ [[3,i,D[i]] for i in range(0,len(D))]
-# print(formatted_codes)
-# AB_score_fleiss = agreement.AnnotationTask(data=formatted_codes)
-# print(AB_score_fleiss.multi_kappa())
+movie_avg_two=avg_fleiss(fleiss_scores_two_movie)
+print("movie avg two: " + str(movie_avg_two))
